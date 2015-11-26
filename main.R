f0b8deba4999b6c7440bf504fc70f5198c1ae8df
Daniel Resende <dresende@verios.com.br>
2015-11-26 21:53:26 -0200

    add verbose and save model

--- main.R
+++ main.R
@@ -1,11 +1,15 @@
+print("loading libraries")
 source('source.R')
 
 # Parameters
 breaks <- 3
 
-train <- read.csv("data/train.csv")
+print("reading data")
+train <- read.csv("data/train.csv", nrows=100)
 # test <- read.csv("data/test.csv", nrows=1000)
 
+print("generating features")
+count <- 1
 x <- apply(train, 1, function(row) {
     matrix <- readImg(row)
     chunks <- ceiling((1:28)/(28/breaks))
@@ -15,6 +19,7 @@ x <- apply(train, 1, function(row) {
     chunkRowMeans <- lapply(rowMeans, mean)
     names(chunkRowMeans) <- paste0("mean_", names(chunkRowMeans))
     chunkRowMeans <- data.frame(chunkRowMeans)
+    
     chunkRowSd <- lapply(rowMeans, sd)
     names(chunkRowSd) <- paste0("sd_", names(chunkRowSd))
     chunkRowSd <- data.frame(chunkRowSd)
@@ -24,20 +29,25 @@ x <- apply(train, 1, function(row) {
     chunkColMeans <- lapply(colMeans, mean)
     names(chunkColMeans) <- paste0("mean_", names(chunkColMeans))
     chunkColMeans <- data.frame(chunkColMeans)
+    
     chunkColSd <- lapply(colMeans, sd)
     names(chunkColSd) <- paste0("sd_", names(chunkColSd))
     chunkColSd <- data.frame(chunkColSd)
+    
+    print(count)
+    count <<- count + 1
     
     data.frame(chunkRowMeans, chunkRowSd, chunkColMeans, chunkColSd)
 })
 x <- do.call(rbind, x)
 x$label <- factor(train$label)
 
-# model <- train(label ~., data=train, method="rf")
+print("machine learning")
 t <- proc.time()
-model <- randomForest(label ~ ., data=x, ntrees=1000)
+model <- randomForest(label ~ ., data=x, ntree=1000)
 model
 print(proc.time() - t)
 
+save(model, file="data/model.rda")
 # predict(model, newdata=test)
 
