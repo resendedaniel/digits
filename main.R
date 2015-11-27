bd721d052c0802593e9871a88f80d841e92bcf21
Daniel Resende <dresende@verios.com.br>
2015-11-27 10:14:52 -0200

    add skewness, kurtosis and aspectRatio

--- main.R
+++ main.R
@@ -5,7 +5,7 @@ source('source.R')
 breaks <- 3
 
 print("reading data")
-train <- read.csv("data/train.csv", nrows=100)
+train <- read.csv("data/train.csv")
 # test <- read.csv("data/test.csv", nrows=1000)
 
 print("generating features")
@@ -15,31 +15,71 @@ x <- apply(train, 1, function(row) {
     chunks <- ceiling((1:28)/(28/breaks))
     
     rowMeans <- rowMeans(matrix)
+    colMeans <- colMeans(matrix)
+    
+    ## Aspect Ratio
+    aspectRatio <- sum(rowMeans > 0) / sum(colMeans > 0)
+    
     rowMeans <- split(rowMeans, paste0("rows_", chunks))
+    colMeans <- split(colMeans, paste0("cols_", chunks))
+    
+    # Rows
+    ## Means
     chunkRowMeans <- lapply(rowMeans, mean)
     names(chunkRowMeans) <- paste0("mean_", names(chunkRowMeans))
     chunkRowMeans <- data.frame(chunkRowMeans)
     
+    ## Sd
     chunkRowSd <- lapply(rowMeans, sd)
     names(chunkRowSd) <- paste0("sd_", names(chunkRowSd))
     chunkRowSd <- data.frame(chunkRowSd)
     
-    colMeans <- colMeans(matrix)
-    colMeans <- split(colMeans, paste0("cols_", chunks))
+    ## Skew
+    chunkRowSkew <- lapply(rowMeans, skewness)
+    names(chunkRowSkew) <- paste0("skew_", names(chunkRowSkew))
+    chunkRowSkew <- data.frame(chunkRowSkew)
+    
+    ## Kurtosis
+    chunkRowKurt <- lapply(rowMeans, kurtosis)
+    names(chunkRowKurt) <- paste0("kurt_", names(chunkRowKurt))
+    chunkRowKurt <- data.frame(chunkRowKurt)
+    
+    # Cols
+    ## Means
     chunkColMeans <- lapply(colMeans, mean)
     names(chunkColMeans) <- paste0("mean_", names(chunkColMeans))
     chunkColMeans <- data.frame(chunkColMeans)
     
+    ## Sd
     chunkColSd <- lapply(colMeans, sd)
     names(chunkColSd) <- paste0("sd_", names(chunkColSd))
     chunkColSd <- data.frame(chunkColSd)
     
+    ## Skew
+    chunkColSkew <- lapply(colMeans, skewness)
+    names(chunkColSkew) <- paste0("skew_", names(chunkColSkew))
+    chunkColSkew <- data.frame(chunkColSkew)
+    
+    ## Kurtosis
+    chunkColKurt <- lapply(colMeans, kurtosis)
+    names(chunkColKurt) <- paste0("kurt_", names(chunkColKurt))
+    chunkColKurt <- data.frame(chunkColKurt)
+    
     print(count)
     count <<- count + 1
     
-    data.frame(chunkRowMeans, chunkRowSd, chunkColMeans, chunkColSd)
+    data.frame(aspectRatio,
+               chunkRowMeans,
+               chunkRowSd,
+               chunkRowSkew,
+               chunkRowKurt,
+               chunkColMeans,
+               chunkColSd,
+               chunkColSkew,
+               chunkColKurt)
 })
 x <- do.call(rbind, x)
+x[sapply(x, is.nan)] <- 0
 x$label <- factor(train$label)
 
 print("machine learning")
